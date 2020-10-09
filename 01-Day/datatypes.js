bedf70269569a45283f44369a5b15e90dd5716ad
Ishola sherifdeen <66917788+boogie009@users.noreply.github.com>
2020-10-09 07:42:58 +0100

    Add files via upload

---
+++ 01-Day/datatypes.js
@@ -0,0 +1,45 @@
+typeof "Boogie";
+typeof 15;
+typeof true;
+typeof undefined;
+typeof null;
+
+let name = "Boogie";
+let number  = 15;
+let marital_status = undefined;
+let isMuslim = true;
+let badCharacter = null;
+// checking data types
+typeof name;
+typeof agedata;
+typeof marital_status;
+typeof isMuslim;
+typeof badCharacter;
+
+// variables without assigning values
+let a;
+let b;
+let c;
+let d;
+
+//variables with assigned values
+let e = 10;
+let f = false;
+let g = "Ishola Sherifdeen";
+let h = [1,2,3,4,5]
+
+
+//variables to store your first name, last name, marital status, country and age on multiple lines
+/*let firstName = "Sherifdeen";
+let lastName = "Ishola";
+let maritalStatus = "Single";
+let country  = "United Kingdom";
+let age  = 10;
+*/
+//variables to store your first name, last name, marital status, country and age in a single line
+let firstName = "Sherifdeen", lastName = "Ishola", maritalStatus = "Single", country = "United Kingdom", age = 10
+
+let myAge = 25;
+let yourAge = 30;
+console.log(`I am ${age} years old.`);
+console.log(`You are ${yourAge} years old.`);
---
+++ 01-Day/variable.js
@@ -0,0 +1,11 @@
+//comments can make readable
+//Welcome to 30DaysOfJavaScript
+/*comments can make code readable,
+easy to reuse and readable*/
+
+
+let name = "Boogie";
+let age  = 15;
+let marital_status = undefined;
+let isMuslim = true;
+let badCharacter = null;
